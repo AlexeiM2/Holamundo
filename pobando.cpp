13796c54fc18cbe3c5bd795e034b1351816786d0
lab <lab@cecasis.local>
2022-11-17 11:20:29 -0500

    Cuca2

---
+++ pobando.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float nota1, nota2, i, n, notafinal;
+	cout<<"\tUNIVERSIDAD POLITECNICA SALESIANA\n";
+	cout<<"\t   Sistema calificaciones\n";
+	cout<<"Ingrese cantidad de alumnos:";
+	cin>>n;
+
+	for (i=1;i<=n;i++){
+	cout<<"\nIngrese nota Primer Parcial: ";
+	cin>>nota1;
+	cout<<"Ingrese nota Segundo Parcial: ";
+	cin>>nota2;
+	notafinal = (nota1+nota2)/2;
+	
+	if (notafinal > 70)
+		{cout<<"Estado: Reprobado\n";
+		}
+	else
+		cout<<"Estado: Aprobado\n";
+	}
+}
+
